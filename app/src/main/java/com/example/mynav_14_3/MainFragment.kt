d9c8def4c758f74e1cad247d4037d7b7f976e8bc
s.kaminskyi <s.kaminskyi@dodotap.com>
2024-02-14 00:23:55 +0200

    upd new 19

--- app/src/main/java/com/example/mynav_14_3/MainFragment.kt
+++ app/src/main/java/com/example/mynav_14_3/MainFragment.kt
@@ -16,6 +16,7 @@ class MainFragment : Fragment() {
     private lateinit var  binding: FragmentMainBinding
     // nb nb nb
     // nb nb nb
+    //new 19
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
